617b19935673b9c9727ff94326999ae16753b1ff
Santiago Enciso <santiago.enciso@q2.com>
2021-04-18 14:05:01 -0500

    Installed styled-components, created HeroSection component and commented Open sans from the font family.

--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -1,18 +1,42 @@
-import React from 'react'
-import logo from "../static/images/logos/logo.svg";
-import { Link } from "react-router-dom";
-import Layout from "../components/layout";
-
+import React from "react";
+import styled from "styled-components";
 function HeroSection() {
-    return (
-        <>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Elizabeth | Design System</h1>
-                <p>Welcome to your new site.</p>
-                <Link to="/page-2/">Go to page 2</Link> <br />
-            </header>
-        </>
-    )
+  return (
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>Elizabeth</Title>
+          <Description>Welcome to your new site.</Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
+  );
 }
-export default HeroSection
+export default HeroSection;
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+`;
+
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 30px;
+`;
+
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`;
+
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`;
+
+const Description = styled.p`
+  font-size: 17px;
+  line-height: 130%;
+`;
