e2e1a537520db94a82c9713a2121c0bf38b3c577
Santiago Enciso <santiago.enciso@q2.com>
2021-04-19 19:46:02 -0500

    Moved styles and sections into the components folder. Created directory for layout files.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import logo from "../static/images/logos/logo.svg"
+import logo from "../static/images/logos/logo.svg";
 // import './App.css';
-import Layout from "../components/layout"
-import { Link } from 'react-router-dom'
-import HeroSection from "../sections/HeroSection";
+import Layout from "../components/layout/layout";
+import { Link } from "react-router-dom";
+import HeroSection from "../components/sections/HeroSection";
 
 function IndexPage() {
-    return (
-        <Layout>
-            <HeroSection/>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <HeroSection />
+    </Layout>
+  );
 }
 
 export default IndexPage;
