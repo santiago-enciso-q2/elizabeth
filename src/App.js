c05bc4487c4f4f8679c35ae2477510d1e6874a48
Santiago Enciso <santiago.enciso@q2.com>
2021-04-20 21:34:41 -0500

    Removed unused variables from components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Layout from "./components/layout/layout";
 import IndexPage from "./pages/index";
 import SecondPage from "./pages/page-2";
 import NotFoundPage from "./pages/404";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
-import logo from "../static/images/logos/logo.svg";
-// import './App.css';
 import Layout from "../components/layout/layout";
-import { Link } from "react-router-dom";
 import HeroSection from "../components/sections/HeroSection";
 
 function IndexPage() {
