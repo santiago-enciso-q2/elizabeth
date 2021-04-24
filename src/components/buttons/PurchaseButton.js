8d6024bd916f1ce6d0b878635cfc7bae9e9af72e
Santiago Enciso <santiago.enciso@q2.com>
2021-04-24 15:15:14 -0500

    Added text wrapper and ring image to PurchaseButton component.

--- src/components/buttons/PurchaseButton.js
+++ src/components/buttons/PurchaseButton.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Caption2, SmallText } from "../styles/TextStyles";
 import { Link } from "react-router-dom";
 import image from "../../static/images/icons/credit.svg";
+import ring from "../../static/images/icons/icon-ring.svg";
 
 export default function PurchaseButton(props) {
   const { title, subtitle } = props;
@@ -11,9 +12,12 @@ export default function PurchaseButton(props) {
       <Wrapper>
         <IconWrapper>
           <Icon src={image} />
+          <Ring src={ring} />
         </IconWrapper>
-        <Title>{title || "Learn to Code"}</Title>
-        <Subtitle>{subtitle || "$19 per month"}</Subtitle>
+        <TextWrapper>
+          <Title>{title || "Learn to Code"}</Title>
+          <Subtitle>{subtitle || "$19 per month"}</Subtitle>
+        </TextWrapper>
       </Wrapper>
     </Link>
   );
@@ -28,8 +32,17 @@ const Wrapper = styled.div`
     0px 20px 40px rgba(23, 0, 102, 0.2),
     inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
   border-radius: 20px;
-  border: 0px;
+  display: grid;
+  grid-template-columns: 53px auto;
+  align-items: center;
+  gap: 20px;
 `;
+
+const TextWrapper = styled.div`
+  display: grid;
+  gap: 4px;
+`;
+
 const Title = styled(Caption2)`
   color: black;
 `;
@@ -46,7 +59,16 @@ const IconWrapper = styled.div`
   display: grid;
   justify-content: center;
   align-content: center;
+  justify-self: center;
+  position: relative;
 `;
+
+const Ring = styled.img`
+  position: absolute;
+  top: -15px;
+  left: -16px;
+`;
+
 const Icon = styled.img`
   width: 29px;
   height: 29px;
