c540d1fcc80cbdfbda6697198c91a6220b83b06e
Santiago Enciso <santiago.enciso@q2.com>
2021-05-10 07:53:04 -0500

    Added animations to mockup divs

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { H1, MediumText } from "../styles/TextStyles";
 import { themes } from "../styles/ColorStyles";
 import PurchaseButton from "../buttons/PurchaseButton";
+import MockupAnimation from "../animations/MockupAnimation";
+
 function HeroSection() {
   return (
     <Wrapper>
@@ -15,6 +17,7 @@ function HeroSection() {
             subtitle="120+ hours of video"
           />
         </TextWrapper>
+        <MockupAnimation />
       </ContentWrapper>
     </Wrapper>
   );
@@ -23,12 +26,17 @@ export default HeroSection;
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  overflow: hidden;
+  /* height added to prevent mockups from getting clipped at the bottom*/
+  height: 800px;
 `;
 
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
+  display: grid;
+  grid-template-columns: 360px auto;
 `;
 
 const TextWrapper = styled.div`
