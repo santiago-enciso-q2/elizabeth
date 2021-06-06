5e9c7397914094e8fa528d106cc768196edb8d99
Santiago Enciso <santiago.enciso@q2.com>
2021-06-06 13:34:33 -0500

    Moved layout.css styling to GlobalStyle.js

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -1,12 +1,45 @@
 import { createGlobalStyle } from "styled-components";
 import { themes, themes as theme } from "./ColorStyles";
+import { normalize } from "polished";
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans');
+  ${normalize()}
+
   body {
+    line-height: 1;
+    font-family: "Nunito", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-size: 16px;
+    /* ##### Google Fonts*/
+    /*font-family: 'Open Sans', sans-serif;*/
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     background: ${theme.light.backgroundColor};
-    
+
     @media (prefers-color-scheme: dark) {
       background: ${themes.dark.backgroundColor};
     }
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  a {
+    color: #3913B8;
+    font-weight: 600;
+    text-decoration: none;
   }
 `;
