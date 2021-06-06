3d4bc7cd8d9a502db81dd90c3150d2d29df50100
Santiago Enciso <santiago.enciso@q2.com>
2021-06-06 15:03:36 -0500

    Added focus, active, and disabled statuses to the button component.

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -15,27 +15,66 @@ const Button = styled.button`
   left: 271px;
   top: 108px;
   border: none;
-  :hover {
+  *,
+  & {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
+  &:hover {
+    border: none;
+    background-color: ${themes.light.button.primaryHoverColor};
+    color: ${themes.light.button.textColorOnPrimary};
     box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
-    0px 30px 60px rgba(23, 0, 102, 0.5),
-    inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
+      0px 30px 60px rgba(23, 0, 102, 0.5),
+      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
     transform: translateY(-3px);
+  }
+
+  &:focus {
+    border: ${themes.light.button.primaryFocusColor} solid 3px;
+    background: ${themes.light.button.primaryColor};
+    color: ${themes.light.button.textColorOnPrimary};
+  }
+
+  &:active {
+    border: none;
+    color: ${themes.light.button.textColorOnPrimary};
+    background: ${themes.light.button.primaryColor};
+  }
 `;
 
 const PrimaryButton = styled(Button)`
   background: ${themes.light.button.primaryColor};
   color: ${themes.light.button.textColorOnPrimary};
+
+  &:disabled {
+    border: none;
+    color: ${themes.light.button.textOnDisabled};
+    background: ${themes.light.button.disabled};
+  }
 `;
 
 const SecondaryButton = styled(Button)`
   background: ${themes.light.button.neutral100};
   border: ${themes.light.button.primaryColor} solid 5px;
   color: ${themes.light.textColorOnPrimary};
+
+  &:disabled {
+    border: ${themes.light.button.disabled} solid 5px;
+    color: ${themes.light.button.disabled};
+    background: ${themes.light.button.neutral100};
+  }
 `;
 
 const TertiaryButton = styled(Button)`
   background: none;
   color: ${themes.light.button.primaryColor};
+
+  &:disabled {
+    border: none;
+    color: ${themes.light.button.disabled};
+    background: ${themes.light.button.neutral100};
+  }
 `;
 
 export { PrimaryButton, SecondaryButton, TertiaryButton };
