39b300096abd671cf3fd4180077422415dfed3f2
Santiago Enciso <santiago.enciso@q2.com>
2021-06-06 13:06:12 -0500

    Updated styling on buttons to use template strings from the themes object.

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -1,36 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 import { themes } from "../styles/ColorStyles";
+import { typeScale, primaryFont } from "../styles/TextStyles";
 
 const Button = styled.button`
   padding: 12px auto;
-  font-size: 1rem;
+  font-size: ${typeScale.paragraph};
   border-radius: 5px;
   min-width: 100px;
   cursor: pointer;
-  font-family: "Nunito", "monospace";
+  font-family: ${primaryFont};
   height: 50px;
   width: 190px;
   left: 271px;
   top: 108px;
+  border: none;
 `;
 
 const PrimaryButton = styled(Button)`
-  background-color: ${themes.dark.backgroundColor};
-  border: none;
-  color: ${themes.dark.textPrimary};
+  background: ${themes.light.button.primaryColor};
+  color: ${themes.light.button.textColorOnPrimary};
 `;
 
 const SecondaryButton = styled(Button)`
-  background-color: ${themes.light.backgroundColor};
-  border: ${themes.light.textPrimary} solid 5px;
-  color: ${themes.light.textPrimary};
+  background: ${themes.light.button.neutral100};
+  border: ${themes.light.button.primaryColor} solid 5px;
+  color: ${themes.light.textColorOnPrimary};
 `;
 
 const TertiaryButton = styled(Button)`
-  background-color: ${themes.light.backgroundColor};
-  border: none;
-  color: ${themes.light.textPrimary};
+  background: none;
+  color: ${themes.light.button.primaryColor};
 `;
 
 export { PrimaryButton, SecondaryButton, TertiaryButton };
