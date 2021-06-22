7d044911d4e63c153ccc1cb6080dbbed1b0acf50
Santiago Enciso <santiago.enciso@q2.com>
2021-06-22 09:28:33 -0500

    Added CloseModalButton component, Illustration and created the SigUpModal component with all the subcomponents in it.

--- src/components/sections/Modal.js
+++ src/components/sections/Modal.js
@@ -2,13 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import { themes } from "../styles/ColorStyles";
 import { typeScale } from "../styles/TextStyles";
+import IllustrationSignUp from "../../static/images/illustrations/makeup-artist.svg";
+import { CloseIcon } from "../../static/images/icons/close";
+import { PrimaryButton } from "../buttons/Buttons";
 
 const ModalWrapper = styled.div`
   width: 800px;
   height: 580px;
   box-shadow: 0px 5px 16px 1px rgba(0, 0, 0, 0.25);
-  background-color: ${(props) => props.themes.formElementBackground};
-  color: ${(props) => props.themes.textOnFormElementBackground};
+  background-color: ${(props) => props.theme.formElementBackground};
+
+  color: ${(props) => props.theme.textOnFormElementBackground};
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -25,3 +29,33 @@ const SignUpText = styled.p`
   max-width: 70%;
   text-align: center;
 `;
+
+const CloseModalButton = styled.button`
+  cursor: pointer;
+  background: none;
+  border: none;
+  position: absolute;
+  right: 40px;
+  top: 40px;
+  width: 24px;
+  height: 24px;
+  padding: 0;
+`;
+
+export const SignUpModal = () => {
+  return (
+    <ModalWrapper>
+      <img
+        src={IllustrationSignUp}
+        alt="Sign up for an account"
+        aria-hidden="true"
+      />
+      <SignUpHeader>Sign Up</SignUpHeader>
+      <SignUpText>Sign up today to get access to cool things!</SignUpText>
+      <PrimaryButton>Sign Up!</PrimaryButton>
+      <CloseModalButton aria-label="Close modal">
+        <CloseIcon />
+      </CloseModalButton>
+    </ModalWrapper>
+  );
+};
