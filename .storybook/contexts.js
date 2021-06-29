d4552674b5fe37bce456c2a4fd04cc4603e1bc43
Santiago Enciso <santiago.enciso@q2.com>
2021-06-29 09:25:02 -0500

    Create context file and imported themes.

---
+++ .storybook/contexts.js
@@ -0,0 +1,23 @@
+import { ThemeProvider } from "styled-components"
+import { themes } from "../src/components/styles/ColorStyles"
+
+export const contexts = [
+    {
+        icon: "box",
+        title: "Themes",
+        components: [ThemeProvider],
+        params: [
+            {
+                name: "Default theme", props: { theme: themes.light, default: true }
+            },
+            {
+                name: "Dark theme", props: { theme: themes.dark}
+            }
+        ],
+        options: {
+            deep: true,
+            disable: false,
+            cancelable: false
+        }
+    }
+]
