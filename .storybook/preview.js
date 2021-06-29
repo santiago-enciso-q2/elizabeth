0f322f8a2bf8596ab025d716c3de6c8ed27f146d
Santiago Enciso <santiago.enciso@q2.com>
2021-06-29 09:27:08 -0500

    Edited preview file and added Buttons.stories.mdx

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,16 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+import { addParameters } from "@storybook/react";
+import { addDecorator } from "@storybook/react";
+import { withContexts } from "@storybook/addon-contexts/react";
+import { contexts } from "./contexts";
+
+
+// Changes below are causing a WSOD
+
+// addParameters({
+//   backgrounds: [
+//     { name: "Default theme", value: "#ffffff", default: true },
+//     { name: "Dark theme", value: "#050449" }
+//   ]
+// });
+
+addDecorator(withContexts(contexts));
