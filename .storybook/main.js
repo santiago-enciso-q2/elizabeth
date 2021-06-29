928a0699e72cf20a0479e2e6049f9f6b8bf6d11d
Santiago Enciso <santiago.enciso@q2.com>
2021-06-29 09:25:35 -0500

    Added more addons to main.js file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,18 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
   ],
   "addons": [
     "@storybook/addon-links",
+    "@storybook/preset-create-react-app",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true
+      }
+    },
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/addon-contexts/register",
+    "@storybook/addon-backgrounds/register"
   ]
 }
