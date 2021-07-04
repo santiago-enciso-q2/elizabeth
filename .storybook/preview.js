f843eaa92f47ce33407db1542297fc990de6a74e
Santiago Enciso <santiago.enciso@q2.com>
2021-07-04 13:17:10 -0500

    Deleted default storybook stories.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,16 +1,16 @@
-import { addParameters } from "@storybook/react";
-import { addDecorator } from "@storybook/react";
+import { addParameters, addDecorator } from "@storybook/react";
 import { withContexts } from "@storybook/addon-contexts/react";
+import { withA11y } from "@storybook/addon-a11y";
+import {withKnobs} from "@storybook/addon-knobs";
 import { contexts } from "./contexts";
 
-
-// Changes below are causing a WSOD
-
-// addParameters({
-//   backgrounds: [
-//     { name: "Default theme", value: "#ffffff", default: true },
-//     { name: "Dark theme", value: "#050449" }
-//   ]
-// });
+addParameters({
+  background: [
+    { name: "Default theme", value: "#ffffff", default: true },
+    { name: "Dark theme", value: "#050449" }
+  ]
+});
 
 addDecorator(withContexts(contexts));
+addDecorator(withKnobs);
+addDecorator(withA11y);
