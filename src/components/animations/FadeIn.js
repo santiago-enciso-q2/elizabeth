8057142c21cee4e09fcbdb959b74289bbf271003
Santiago Enciso <santiago.enciso@q2.com>
2021-07-09 13:46:58 -0500

    Removed Button components from HeroSection.js

--- src/components/animations/FadeIn.js
+++ src/components/animations/FadeIn.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
-import { animated, useSpring } from "react-spring";
+import React, {useState} from "react";
+import {animated, useSpring} from "react-spring";
 
 const FadeIn = () => {
-  const [showText, setShowText] = useState(true);
-  const animation = useSpring({
-    opacity: showText ? 1 : 0,
-    transform: showText ? `translateY(0)` : `translateY(-30%)`,
-  });
-  return (
-    <div>
-      <button onClick={() => setShowText(!showText)}>Toggle Animation</button>
-      <animated.p style={animation}>Animate me on click!</animated.p>
-    </div>
-  );
+    const [showText, setShowText] = useState(true);
+    const animation = useSpring({
+        opacity: showText ? 1 : 0,
+        transform: showText ? `translateY(0)` : `translateY(-30%)`,
+    });
+    return (
+        <div>
+            <button onClick={() => setShowText(!showText)}>Toggle Animation</button>
+            <animated.p style={animation}>Animate me on click!</animated.p>
+        </div>
+    );
 };
 
 export default FadeIn;
