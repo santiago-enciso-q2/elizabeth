333f4b3d5d3550e9c24eef97edb46586ca25577e
Santiago Enciso <santiago.enciso@q2.com>
2021-07-09 13:48:15 -0500

    Moved Button components to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,95 @@
 import Layout from "../components/layout/layout";
-import HeroSection from "../components/sections/HeroSection";
+import React, { useState } from "react";
+import { ThemeProvider} from "styled-components";
+import { themes } from "../components/styles";
+import { H1} from "../components/styles";
+import { SignUpModal } from "../components/sections/Modal";
+import FadeIn from "../components/animations/FadeIn";
+
+import {
+    PrimaryButton,
+    SecondaryButton,
+    TertiaryButton,
+} from "../components/buttons/Buttons";
+
 
 function IndexPage() {
-  return (
-    <Layout>
-      <HeroSection />
-    </Layout>
-  );
+    const [useDarkTheme, setUseDarkTheme] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    return (
+        <ThemeProvider theme={useDarkTheme ? themes.dark : themes.light}>
+            <Layout>
+
+                {" "}
+                <PrimaryButton onClick={() => setUseDarkTheme(true)}>
+                    Dark Theme
+                </PrimaryButton>
+                <SecondaryButton onClick={() => setUseDarkTheme(false)}>
+                    Default Theme
+                </SecondaryButton>
+                <div
+                    style={{
+                        width: "100vw",
+                        height: "45vh",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-around",
+                    }}
+                >
+                    {" "}
+                    <H1> Default Buttons</H1>
+                    <PrimaryButton>One </PrimaryButton>
+                    <SecondaryButton>Two</SecondaryButton>
+                    <TertiaryButton>Three</TertiaryButton>
+                </div>
+                <hr />
+                <H1>Button Combinations</H1>
+                <p>Primary Buttons</p>
+                <PrimaryButton modifiers={["small", "success", "primaryButtonSuccess"]}>
+                    Hello world
+                </PrimaryButton>
+                <PrimaryButton modifiers={["warning", "primaryButtonWarning"]}>
+                    Hello world
+                </PrimaryButton>
+                <PrimaryButton modifiers={["large", "error", "primaryButtonError"]}>
+                    Hello world
+                </PrimaryButton>
+                <p>Secondary Buttons</p>
+                <SecondaryButton
+                    modifiers={["small", "success", "secondaryButtonSuccess"]}
+                >
+                    Goodbye world
+                </SecondaryButton>
+                <SecondaryButton modifiers={["warning", "secondaryButtonWarning"]}>
+                    Goodbye world
+                </SecondaryButton>
+                <SecondaryButton modifiers={["large", "error", "secondaryButtonError"]}>
+                    Goodbye world
+                </SecondaryButton>
+                <p>Tertiary Buttons</p>
+                <TertiaryButton modifiers={["success", "tertiaryButtonSuccess"]}>
+                    Hey world
+                </TertiaryButton>
+                <TertiaryButton modifiers={["warning", "tertiaryButtonWarning"]}>
+                    Hey world
+                </TertiaryButton>
+                <TertiaryButton modifiers={["error", "tertiaryButtonError"]}>
+                    Hey world
+                </TertiaryButton>
+                <hr />
+                <H1>Modals</H1>
+                <PrimaryButton onClick={() => setShowModal(!showModal)}>
+                    Toggle Modal
+                </PrimaryButton>
+                <SignUpModal showModal={showModal} setShowModal={setShowModal} />
+                <hr />
+                <H1>Animations</H1>
+                <FadeIn />
+                <hr />
+            </Layout>
+        </ThemeProvider>
+
+    );
 }
 
 export default IndexPage;
